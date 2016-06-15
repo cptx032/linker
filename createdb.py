559d62d69c85beebdde961950e81cf48aa66560f
Willie Lawrence <cptx032@gmail.com>
2016-06-15 16:21:59 -0300

    #13 - Making "createdb" script

---
+++ createdb.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author: Willie Lawrence - cptx032 arroba gmail dot com
+import sys
+import sqlite3
+from models import User, Elem, FOLDER, LINK
+
+ELEM_TABLE = '''
+CREATE TABLE ELEM (
+	ID INTEGER PRIMARY KEY,
+	NAME TEXT NOT NULL,
+	TYPE INTEGER NOT NULL,
+	DESCRIPTION TEXT NOT NULL,
+	PARENT INTEGER
+)
+'''
+
+USER_TABLE = '''
+CREATE TABLE USER (
+	ID INTEGER PRIMARY KEY,
+	NAME TEXT NOT NULL,
+	AUTHSTRING TEXT
+)
+'''
+
+try:
+	Elem.get_by_id(1)
+except sqlite3.OperationalError:
+	print 'tables not found...creating tables'
+	connection = sqlite3.connect('./db.db')
+	cursor = connection.cursor()
+	cursor.execute(ELEM_TABLE)
+	cursor.execute(USER_TABLE)
+	connection.commit()
+	connection.close()
+
+user_key = raw_input('Enter user key: ')
+see = raw_input('It can see links? (y/n) ').lower() == 'y'
+add = raw_input('It can add links? (y/n) ').lower() == 'y'
+delete = raw_input('It can delete links?(y/n) ').lower() == 'y'
+edit = raw_input('It can edit links? (y/n) ').lower() == 'y'
+
+permission = ''
+if see:
+	permission += 's'
+if add:
+	permission += 'a'
+if delete:
+	permission += 'd'
+if edit:
+	permission += 'e'
+
+User.insert(User(None, user_key, permission))
+
+if not Elem.get_by_id(1):
+	print 'No root element found...creating root folder'
+	Elem.insert(Elem(None, 'Root', FOLDER, 'root', None))
---
+++ models.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+# Author: Willie Lawrence - cptx032 arroba gmail dot com
+import sqlite3
+
+class User:
+	def __init__(self, id, name, authstring):
+		self.id = id
+		self.name = name
+		self.authstring = authstring
+	
+	@staticmethod
+	def get_user_by_name(name):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''SELECT * FROM USER WHERE NAME=?'''
+		cursor.execute(sql, (name,))
+		row = cursor.fetchone()
+		
+		connection.close()
+		
+		if not row:
+			return None
+		return User(*row)
+	
+	@staticmethod
+	def get_all(name):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		sql = '''SELECT * FROM USER'''
+		cursor.execute(sql)
+		row = cursor.fetchone()
+		connection.close()
+		if not row:
+			return None
+		return User(*row)
+	
+	@staticmethod
+	def insert(elem):
+		sql = '''INSERT INTO USER (NAME, AUTHSTRING)
+		VALUES (?,?)
+		'''
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		cursor.executemany(sql, [(elem.name, elem.authstring)])
+		
+		connection.commit()
+		connection.close()
+	
+	def update(self):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''UPDATE USER SET NAME=?, AUTHSTRING=? WHERE ID=?'''
+		cursor.execute(sql, (self.name, self.authstring, self.id))
+		
+		connection.commit()
+		connection.close()
+
+	def remove(self):
+		childs = self.get_elems()
+		for i in childs:
+			i.remove()
+		# removing it self
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''DELETE FROM USER WHERE ID=?'''
+		cursor.execute(sql, (self.id,))
+		
+		connection.commit()
+		connection.close()
+
+FOLDER = 1
+LINK = 2
+class Elem:
+	def __init__(self, id, name, type, desc, parent=None):
+		assert(type in [FOLDER, LINK])
+		self.id = id
+		self.type = type
+		self.name = name
+		self.desc = desc
+		self.parent = parent
+	
+	def get_parent(self):
+		if self.parent:
+			return Elem.get_by_id(self.parent)
+		else:
+			return None
+
+	def update(self):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''UPDATE ELEM SET NAME=?, TYPE=?, DESCRIPTION=? WHERE ID=?'''
+		cursor.execute(sql, (self.name, self.type, self.desc, self.id))
+		
+		connection.commit()
+		connection.close()
+	
+	def get_elems(self):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		sql = '''SELECT * FROM ELEM WHERE PARENT=?'''
+		return [Elem(*i) for i in cursor.execute(sql, (self.id,))]
+		connection.close() # fixme
+
+	def remove(self):
+		childs = self.get_elems()
+		for i in childs:
+			i.remove()
+		# removing it self
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''DELETE FROM ELEM WHERE ID=?'''
+		cursor.execute(sql, (self.id,))
+		
+		connection.commit()
+		connection.close()
+
+	@staticmethod
+	def insert(elem):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		sql = '''INSERT INTO ELEM (NAME, TYPE, DESCRIPTION, PARENT)
+		VALUES (?,?,?,?)
+		'''
+		cursor.executemany(sql, [(elem.name, elem.type, elem.desc, elem.parent)])
+		
+		connection.commit()
+		connection.close()
+
+	@staticmethod
+	def get_by_id(id):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''SELECT * FROM ELEM WHERE ID=?'''
+		cursor.execute(sql, (id,))
+		row = cursor.fetchone()
+		
+		connection.close()
+		if not row:
+			return None
+		return Elem(*row)
+
+	@staticmethod
+	def search_by_name_description(name, description):
+		connection = sqlite3.connect('./db.db')
+		cursor = connection.cursor()
+		
+		sql = '''SELECT * FROM ELEM WHERE NAME=? AND DESCRIPTION=?'''
+		cursor.execute(sql, (name, description))
+		row = cursor.fetchone()
+		connection.close()
+		if not row:
+			return None
+		return Elem(*row)
